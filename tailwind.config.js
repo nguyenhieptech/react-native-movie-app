818c321208fad26b6ce46dba5ae2c8551c1a858d
Hiep Nguyen <nguyenhieptech@gmail.com>
2022-11-02 17:46:35 +0700

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./App.{ts,tsx}', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00DAC0',
+        mistake1: '#FAEBEB',
+        mistake2: '#E64646',
+        right: '#4BB34B',
+      },
+    },
   },
   plugins: [],
 };
