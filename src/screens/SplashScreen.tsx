3ffb362384ec29b38565e5da2f009ff712e627ac
Hiep Nguyen <nguyenhieptech@gmail.com>
2022-11-18 13:15:56 +0700

    Add mock screens

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function SplashScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-800">
+      <Text className="text-slate-200">Splash Screen</Text>
+    </View>
+  );
+}
---
+++ src/screens/download/DownloadScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export function DownloadScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-800">
+      <Text className="text-slate-200">Download Screen</Text>
+    </View>
+  );
+}
