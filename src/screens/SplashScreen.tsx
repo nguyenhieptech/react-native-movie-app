bf1a6ba1a6fa98f01ea27615f739cb232fa7adbf
Hiep Nguyen <nguyenhieptech@gmail.com>
2022-11-23 00:31:04 +0700

    Add SplashScreen in CommonScreensStack

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,10 +1,31 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { AppStackParamList } from 'src/navigation/types';
 
-export default function SplashScreen() {
+export function SplashScreen({
+  navigation,
+}: NativeStackScreenProps<AppStackParamList, 'Splash'>) {
   return (
     <View className="flex-1 items-center justify-center bg-slate-800">
-      <Text className="text-slate-200">Splash Screen</Text>
+      <Text className="mt-4 text-4xl font-bold text-slate-100">
+        Splash Screen
+      </Text>
+      <TouchableOpacity
+        className="mt-6"
+        onPress={() =>
+          navigation.navigate('MainBottomTabs', {
+            screen: 'HomeStack',
+            params: {
+              screen: 'HomeHistory',
+            },
+          })
+        }
+      >
+        <Text className="text-center text-white">
+          Demo go to specific screen inside HomeStack/HomeHistoryScreen
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
