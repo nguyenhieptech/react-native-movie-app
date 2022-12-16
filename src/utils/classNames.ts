fa7ddbb1cf5886f39de81c761172ad9f9571aa5a
Hiep Nguyen <nguyenhieptech@gmail.com>
2022-12-16 08:43:40 +0700

    Update and add utils

--- src/utils/classNames.ts
+++ src/utils/classNames.ts
@@ -1,5 +1,7 @@
 /**
  * Helper for long className and conditional className
+ *
+ * @see https://tailwindui.com/components/marketing/elements/headers
  */
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
