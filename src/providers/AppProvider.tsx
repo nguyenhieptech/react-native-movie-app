87246fc41c538c0a89fad7190262b7f18253a16a
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-01-22 09:58:09 +0700

    Update comment

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 import { GestureHandlerProvider } from './GestureHandlerProvider';
 
-/** Wrap all providers inside this AppProvider */
+/** Wrap all providers or React contexts inside this AppProvider. */
 export function AppProvider({ children }: PropsWithChildren) {
   return <GestureHandlerProvider>{children}</GestureHandlerProvider>;
 }
